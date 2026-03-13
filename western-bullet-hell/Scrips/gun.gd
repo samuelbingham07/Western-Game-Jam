0bc6f71435ff2ad807b5b1165aac511d96f3fd89
RealmOfShaows <46869020+RealmOfShaows@users.noreply.github.com>
2026-03-13 15:31:24 -0700

    Gun Rotation WIP

--- western-bullet-hell/Scrips/gun.gd
+++ western-bullet-hell/Scrips/gun.gd
@@ -11,12 +11,21 @@ func _ready() -> void:
 
 func _process(delta: float) -> void:
 	look_at(get_global_mouse_position())
-	if rotation > 270:
-		pass
+	
+	fposmod(rotation_degrees, 360)
+	
+	if rotation_degrees > 90 and rotation_degrees < 270:
+		#animated_sprite_2d.flip_h = true
+		scale.y = -1
+	else:
+		#animated_sprite_2d.flip_h = false
+		scale.y = 1
+		
+	print(rotation_degrees)
 	
 	if Input.is_action_just_pressed("shoot"):
+		animated_sprite_2d.play("shoot")
 		var bullet_instance = Bullet.instantiate()
 		get_tree().root.add_child(bullet_instance)
 		bullet_instance.global_position = global_position
 		bullet_instance.rotation = rotation
-		animated_sprite_2d.play("shoot")
