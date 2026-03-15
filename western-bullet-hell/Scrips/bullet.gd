f92461a1021bf6da7fd96ebd725ba77c64319550
samuelbingham07 <samuelbingham07@gmail.com>
2026-03-15 15:17:01 -0700

    change layers

--- western-bullet-hell/Scrips/bullet.gd
+++ western-bullet-hell/Scrips/bullet.gd
@@ -2,14 +2,24 @@ extends Node2D
 
 const SPEED := 300
 var speed_multiplier := 1.0
+var from_player := false  # set to true when player shoots
 
 func _ready() -> void:
 	$Area2D/CollisionShape2D.set_deferred("disabled", true)
-	await get_tree().create_timer(0.3).timeout
+	await get_tree().create_timer(0.2).timeout
 	$Area2D/CollisionShape2D.set_deferred("disabled", false)
 
 func _process(delta: float) -> void:
 	position += transform.x * SPEED * speed_multiplier * delta
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
+	var parent = area.get_parent()
+	if from_player and parent.has_method("take_damage"):
+		parent.take_damage(1)
+		queue_free()
+	elif not from_player and parent.has_method("damage_dealt"):
+		parent.damage_dealt(1)
+		queue_free()
+
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	queue_free()
--- western-bullet-hell/Scrips/gun.gd
+++ western-bullet-hell/Scrips/gun.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 var gun_jammed = false
 var available_bullets = 0
+
 @export var max_bullets = 6
 @onready var Bullet = preload("res://Scenes/bullet.tscn")
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
@@ -25,6 +26,7 @@ func _process(delta: float) -> void:
 
 	if Input.is_action_just_pressed("shoot") and available_bullets > 0:
 		var bullet_instance = Bullet.instantiate()
+		bullet_instance.from_player = true  
 		bullet_instance.speed_multiplier = 1.0 
 		get_tree().root.add_child(bullet_instance)
 		bullet_instance.global_position.x = global_position.x + 10
