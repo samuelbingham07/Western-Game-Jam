21461fd722d8483989440aebc847a4a08036c27c
samuelbingham07 <samuelbingham07@gmail.com>
2026-03-15 13:34:05 -0700

    enemy script to move towards player

--- western-bullet-hell/Scrips/enemy.gd
+++ western-bullet-hell/Scrips/enemy.gd
@@ -1,48 +1,19 @@
 extends CharacterBody2D
 
+@export var speed := 100.0
+var player: Node2D = null
+
+func _ready() -> void:
+	await get_tree().process_frame
+	player = get_tree().root.find_child("CharacterBody2D", true, false)
+	print("final player = ", player)
+
+func _physics_process(delta: float) -> void:
+	if player == null:
+		return
+	var direction = (player.global_position - global_position).normalized()
+	velocity = direction * speed
+	move_and_slide()
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	queue_free()
-	
-#var speed = 30
-#
-#var enemy_velocity = Vector2.ZERO
-#
-#var player = get_tree().get_first_node_in_group("player")
-#
-#var randomnum
-#
-#var target
-#
-#enum {
-	#SURROUND, ATTACK, HIT
-#}
-#
-#var state = SURROUND
-#
-#func _ready():
-	#var rng = RandomNumberGenerator.new()
-	#rng.randomize()
-	#randomnum = rng.randf()
-	#target = get_circle_position(randomnum)
-#
-#func _physics_process(delta):
-	#match state:
-		#SURROUND:
-			#move(target, delta)
-			#
-#func get_circle_position(random):
-	#var kill_circle_center = player.global_position
-	#var radius = 40
-	#var angle = random * PI * 2
-	#var x = kill_circle_center.x + cos(angle) + radius
-	#var y = kill_circle_center.y + sin(angle) + radius
-	#
-	#return Vector2(x,y)
-#
-#func move(target, delta):
-	#var direction = (target - global_position.normalized())
-	#var desired_velocity = direction * speed
-	#var steering = (desired_velocity - enemy_velocity) * delta * 2.5
-	#enemy_velocity += steering
-	#enemy_velocity = move_and_slide()s
--- western-bullet-hell/Scrips/spawner.gd
+++ western-bullet-hell/Scrips/spawner.gd
@@ -3,23 +3,16 @@ extends Node2D
 var width := 1152
 var height := 648
 var entities := 0
-
 var spawnable_entities := [
 	preload("res://Scenes/coin.tscn"),
 	preload("res://Scenes/enemy.tscn"),
-	
 ]
-
 @export var max_entities := 6
-
 @onready var timer = $Timer
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	timer.wait_time = randi_range(1, 2)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	print(entities)
 
@@ -27,19 +20,22 @@ func remove_entities():
 	if entities > 0:
 		entities -= 1
 
-
 func _on_timer_timeout() -> void:
 	var nwidth = randi_range(1, width)
 	var nheight = randi_range(1, height)
 	var spawned = spawnable_entities.pick_random()
 	var spawn = spawned.instantiate()
-		
+
 	if entities < max_entities:
 		spawn.global_position = Vector2(nwidth, nheight)
 		add_child(spawn)
 		timer.wait_time = randi_range(1, 2)
 		entities += 1
-	
+
+		# Pass the player reference to enemies after adding to scene
+		if spawn.has_method("set_player"):
+			var player = get_tree().get_first_node_in_group("player")
+			spawn.set_player(player)
+
 	spawn.tree_exited.connect(remove_entities)
 	
-# hi hi hi
