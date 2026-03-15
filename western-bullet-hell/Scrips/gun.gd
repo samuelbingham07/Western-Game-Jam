74ee4276212d7837c7a521f08b9ab1686e9cbdbb
RealmOfShaows <46869020+RealmOfShaows@users.noreply.github.com>
2026-03-15 10:49:41 -0700

    Gun Rotation

--- western-bullet-hell/Scrips/gun.gd
+++ western-bullet-hell/Scrips/gun.gd
@@ -13,11 +13,12 @@ func _ready() -> void:
 
 func _process(delta: float) -> void:
 	look_at(get_global_mouse_position())
-	if global_rotation < -4 and global_rotation > 1:
-		animated_sprite_2d.flip_h = true
+	
+	if get_global_mouse_position().x < global_position.x:
+		scale.y = -1
 	else:
-		animated_sprite_2d.flip_h = false
-		
+		scale.y = 1
+	
 	print(global_rotation)
 	
 	if Input.is_action_just_pressed("shoot") and available_bullets < 1:
@@ -26,7 +27,8 @@ func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("shoot") and available_bullets > 0:
 		var bullet_instance = Bullet.instantiate()
 		get_tree().root.add_child(bullet_instance)
-		bullet_instance.global_position = global_position
+		bullet_instance.global_position.x = global_position.x + 10
+		bullet_instance.global_position.y = global_position.y - 10
 		bullet_instance.rotation = rotation
 		animated_sprite_2d.play("shoot")
 		available_bullets -= 1
