2cc183c98350c246f01b15791f31f25e43d5e868
RealmOfShaows <46869020+RealmOfShaows@users.noreply.github.com>
2026-03-15 16:09:25 -0700

    Barrel Bad Things

--- western-bullet-hell/Scrips/barrel.gd
+++ western-bullet-hell/Scrips/barrel.gd
@@ -4,7 +4,7 @@ extends Node2D
 @onready var area_2d: Area2D = $Area2D
 @onready var deletion: Timer = $Deletion
 
-
+var bad_things: Array[Callable] = [ speed_down, max_bullet_down]
 
 
 # Called when the node enters the scene tree for the first time.
@@ -17,18 +17,17 @@ func _process(delta: float) -> void:
 	pass
 
 
-
-func _on_area_2d_area_entered(area: Area2D) -> void:
-	animated_sprite_2d.play("opening")
-	area_2d.queue_free()
-	deletion.start()
-	max_bullet_down()
-
-func _on_deletion_timeout() -> void:
-	queue_free()
-
 func speed_down():
 	Global.player_speed = 100
 
 func max_bullet_down():
 	Global.max_bullets = 2
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	animated_sprite_2d.play("opening")
+	area_2d.queue_free()
+	deletion.start()
+	bad_things.pick_random().call()
+
+func _on_deletion_timeout() -> void:
+	queue_free()
