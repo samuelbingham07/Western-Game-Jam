db0434181f1090f43bb7f65721fc0036b4f7d88b
samuelbingham07 <samuelbingham07@gmail.com>
2026-03-15 18:20:00 -0700

    scene change

--- western-bullet-hell/Scrips/player.gd
+++ western-bullet-hell/Scrips/player.gd
@@ -1,41 +1,33 @@
 extends CharacterBody2D
 
 var screen_size
-
 @onready var c_label = $Coins
 @onready var h_label = $Health
-@onready var restart = $RestartTimer
 @onready var area = $Area2D
 @onready var character = $"."
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 @onready var gun: Node2D = $Gun
 @onready var modulator: Timer = $Modulator
-
-
+@onready var death_screen = $CanvasLayer
+@onready var death_label = $CanvasLayer/Label
+@onready var sub_label = $CanvasLayer/Label2  # ✅ Added
+@onready var retry_button = $CanvasLayer/Button
 
 const ACCEL := 10.0
 const WRAPPING := 40
-
 signal health_zero
 
 func _ready() -> void:
 	screen_size = get_viewport_rect().size
 	health_zero.connect(_on_health_zero)
 	animated_sprite_2d.play("walk_down")
+	death_screen.visible = false
 
 func _process(delta: float) -> void:
-	
-	#Carries the value of the Input Vector
 	var playerInput = player_movement()
-	
-	#Allows the player to move smoothly by lerping the starting
-	#point (velocity) to where the player is moving to (PI) by
-	# a factor of ACCEL * delta
 	velocity = lerp(velocity, playerInput * Global.player_speed, ACCEL * delta)
 	move_and_slide()
-	
-	
-	
+
 	if playerInput == Vector2(1.0, 0.0):
 		animated_sprite_2d.play("walk_right")
 		gun.global_position.x = global_position.x + 10
@@ -51,41 +43,24 @@ func _process(delta: float) -> void:
 	if playerInput == Vector2(0.0, -1.0):
 		animated_sprite_2d.play("walk_up")
 		gun.visible = false
-	
-	
-	#wrapping()
+
 	clamp_to_borders()
 	coin_label()
 	health_label()
 
 func player_movement():
-	#Assigns values of -1 and 1 to Input button maps (always normalized))
 	var direction = Input.get_vector("left", "right", "up", "down")
-	#Returns the value of ^ to be used in other functions
 	return direction
-	
+
 func clamp_to_borders():
 	position.x = clamp(position.x, 50, screen_size.x - 50)
 	position.y = clamp(position.y, 100, screen_size.y - 125)
 
-	
-#func wrapping():
-	#if position.x > screen_size.x + WRAPPING:
-		#position.x = -WRAPPING
-	#elif position.x < screen_size.x - screen_size.x - WRAPPING:
-		#position.x = screen_size.x + WRAPPING
-	#
-	#if position.y > screen_size.y + WRAPPING:
-		#position.y = -WRAPPING
-	#elif position.y < screen_size.y - screen_size.y - WRAPPING:
-		#position.y = screen_size.y + WRAPPING
-	
 func coin_label():
 	c_label.text = "Coins: " + str(Global.coins)
-	
+
 func health_label():
 	h_label.text = "Health: " + str(Global.p_health)
-
 
 func damage_dealt(amount):
 	Global.p_health -= amount
@@ -93,18 +68,26 @@ func damage_dealt(amount):
 	modulator.start()
 	if Global.p_health <= 0:
 		health_zero.emit()
-	
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	damage_dealt(1)
-	
-func _on_health_zero():
-	print("You are Dead")
-	character.visible = false
-	area.queue_free() 
-	restart.start()
 
-func _on_restart_timer_timeout() -> void:
+func _on_health_zero():
+	character.visible = false
+	area.queue_free()
+	death_label.text = "You Died!"
+	sub_label.text = "Better luck next time..."  # ✅ Death message
+	retry_button.text = "Retry"
+	death_screen.visible = true
+
+func show_win_screen():
+	character.visible = false
+	death_label.text = "You Win! 🎉"
+	sub_label.text = "Congratulations!"  # ✅ Win message
+	retry_button.text = "Play Again"
+	death_screen.visible = true
+
+func _on_button_pressed() -> void:
 	Global.coins = 0
 	Global.p_health = 10
 	get_tree().reload_current_scene()
